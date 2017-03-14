edb22ed314296a065df55c51c8e41ff25b6b36e7
大畔祐輝 <ooguro@ooazeyuuakiranoMacBook-Pro.local>
2017-03-14 21:02:12 +0900

    firefox立ち上がる

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,10 +1,25 @@
+import org.fluentlenium.adapter.FluentStandalone;
 import org.fluentlenium.adapter.junit.FluentTest;
 import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.fluentlenium.core.filter.FilterConstructor.with;
 
 public class MyTest extends FluentTest {
     @Test
-    public void testGoogle() {
-        System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
-        goTo("http://www.google.com");
+    public void title_of_duck_duck_go_should_contain_search_query_name() {
+        System.setProperty("webdriver.gecko.driver", "./exe/geckodriver");
+        goTo("https://duckduckgo.com");
+        $("#search_form_input_homepage").fill().with("FluentLenium");
+        $("#search_button_homepage").submit();
+        assertThat(window().title()).contains("FluentLenium");
     }
+
+    @Override
+    public String getWebDriver() {
+        return "firefox";
+    }
+
 }
