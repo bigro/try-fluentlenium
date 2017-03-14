7da122a1b6698c8c8564e92e796f79dcb443eebe
Yuuki Ooguro <kitkat.jaja@gmail.com>
2017-03-14 11:48:16 +0900

    もろもろバージョンあげてgeckodriverセットしてみた

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 public class MyTest extends FluentTest {
     @Test
     public void testGoogle() {
+        System.setProperty("webdriver.gecko.driver", "geckodriver.exe");
         goTo("http://www.google.com");
     }
 }
