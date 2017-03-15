feeeef2a41aff6896ec8d0b0015d5613271b240d
Yuuki Ooguro <kitkat.jaja@gmail.com>
2017-03-15 12:29:40 +0900

    winとmacの判定を入れた

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -6,6 +6,9 @@ import java.io.File;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MyTest extends FluentTest {
+    private static final boolean WIN = File.separatorChar == '\\';
+    private String driverPath;
+
     @Test
     public void title_of_duck_duck_go_should_contain_search_query_name() {
         goTo("https://duckduckgo.com");
@@ -16,7 +19,12 @@ public class MyTest extends FluentTest {
 
     @Override
     public String getWebDriver() {
-        File file = new File("src/exe/chromedriver.exe");
+        if (WIN) {
+            driverPath = "src/exe/chromedriver.exe";
+        } else {
+            driverPath = "src/exe/chromedriver";
+        }
+        File file = new File(driverPath);
         System.setProperty("webdriver.chrome.driver", file.getPath());
         return "chrome";
     }
