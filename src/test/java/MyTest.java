ad9c18df54645e77196ed93c949d7301cbbf3594
Yuuki Ooguro <kitkat.jaja@gmail.com>
2017-03-15 12:25:03 +0900

    windowsでchromeを動かせる

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,12 +1,13 @@
 import org.fluentlenium.adapter.junit.FluentTest;
 import org.junit.Test;
 
+import java.io.File;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MyTest extends FluentTest {
     @Test
     public void title_of_duck_duck_go_should_contain_search_query_name() {
-        System.setProperty("webdriver.gecko.driver", "./exe/geckodriver");
         goTo("https://duckduckgo.com");
         $("#search_form_input_homepage").fill().with("FluentLenium");
         $("#search_button_homepage").submit();
@@ -15,7 +16,9 @@ public class MyTest extends FluentTest {
 
     @Override
     public String getWebDriver() {
-        return "firefox";
+        File file = new File("src/exe/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", file.getPath());
+        return "chrome";
     }
 
 }
